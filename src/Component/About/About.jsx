f930179cbda73d8560fe8214c94035e70b573e16
marwanabdelwahab9@gmail.com <marwanabdelwahab9@gmail.com>
2023-03-02 08:15:37 +0200

    First commit

---
+++ src/Component/About/About.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./About.css";
+export default function About() {
+  return (
+    <div className="about text-white">
+      <h2 className=" d-flex justify-content-center my-4 ">
+        MARWAN ABDELWAHAB
+      </h2>
+      <h4 className="d-flex justify-content-center">I AM WEB</h4>
+      <h3 className="d-flex justify-content-center">
+        <span className="text-danger">DESIGNER </span> &{" "}
+        <span className="text-primary"> DEVELOPER</span>
+      </h3>
+
+      <p className="lead d-flex justify-content-center">
+        Specialized in front end web development
+      </p>
+      <h6 className="">
+        <span className="d-flex justify-content-center">
+          Have an experience in web designing and development{" "}
+        </span>{" "}
+        <br /> building high quality & responsive websites using HTML, CSS,
+        Bootstrap, <br />
+        <span className="d-flex justify-content-center">
+          Tailwind CSS, JavaScript and React JS.
+        </span>
+      </h6>
+      <div className="d-flex justify-content-center my-4">
+        {" "}
+        <button className=" btn btn-danger text-white border-5  ">
+          Download CV{" "}
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/Component/Nav/Nav.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./Nav.css";
+export default function Nav() {
+  return (
+    <div>
+      <nav className="navbar fixed-top bg-transparent  navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse " id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  PROJECTS
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  CONTACT
+                </a>
+              </li>
+              <li className="nav-item d-flex">
+                <a
+                  className="nav-link text-dark mx-1 fs-5 fw-bolder"
+                  target="_blank"
+                  href="https://github.com/mrwan14"
+                >
+                  <i class="fa-brands fa-github-alt"></i>
+                </a>
+                <a
+                  className="nav-link text-dark fs-5 fw-bolder"
+                  target="_blank"
+                  href="https://linkedin.com/in/marwan-abdelwahab-448593209"
+                >
+                  <i class="fa-brands fa-linkedin"></i>
+                </a>
+              </li>
+              {/* <li className="nav-item">
+                <a
+                  className="nav-link text-dark fs-5 fw-bolder"
+                  target="_blank"
+                  href="https://github.com/mrwan14"
+                >
+                  <i class="fa-brands fa-github-alt"></i>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link text-dark fs-5 fw-bolder"
+                  target="_blank"
+                  href="https://linkedin.com/in/marwan-abdelwahab-448593209"
+                >
+                  <i class="fa-brands fa-linkedin"></i>
+                </a>
+              </li> */}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
